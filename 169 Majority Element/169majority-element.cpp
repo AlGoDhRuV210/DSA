03e440a95c0c4c1e11d11314ad2440f030455cd1
Dhruv Kumar Gupta <dhruvkumargupta@Dhruvs-MacBook-Air-M4.local>
2026-03-22 09:57:58 +0000

    Code challenge submission for question: 169 Majority Element

---
+++ 169 Majority Element/169majority-element.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int ele = nums[0];
+        int cnt = 1;
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] == ele)
+            {
+                cnt++;
+            }
+            else if (nums[i] != ele && cnt != 0)
+                cnt--;
+            else if (cnt == 0)
+            {
+                ele = nums[i];
+                cnt++;
+            }
+        }
+        return ele;
+    }
+};
