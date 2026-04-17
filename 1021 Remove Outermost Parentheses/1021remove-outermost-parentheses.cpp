5037d8f0d95bcb2e0d6ce7cdfdac028cbce3cc97
Dhruv Kumar Gupta <dhruvkumargupta@Dhruvs-MacBook-Air-M4.local>
2026-04-17 14:27:38 +0000

    Code challenge submission for question: 1021 Remove Outermost Parentheses

---
+++ 1021 Remove Outermost Parentheses/1021remove-outermost-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) 
+    {
+        string ans="";
+        int cnt=0;
+        for(auto i : s)
+        {
+            if(i=='(')
+            {
+                if(cnt>0) ans+=i;
+                cnt++;
+            }
+            else
+            {
+                cnt--;
+                if(cnt>0) ans+=i;
+            }
+
+        }
+        return ans;
+        
+        
+
+    }
+};
