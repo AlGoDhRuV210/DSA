9c251105848ef684a5931cccaaf191b91405e805
Dhruv Kumar Gupta <dhruvkumargupta@Dhruvs-MacBook-Air-M4.local>
2026-05-17 18:32:02 +0000

    Code challenge submission for question: 1614 Maximum Nesting Depth of the Parentheses

---
+++ 1614 Maximum Nesting Depth of the Parentheses/1614maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int ans=0;
+        int cnt=0;
+        for(auto c : s)
+        {
+            if(c=='(') {cnt++; ans=max(ans,cnt);}
+            else if (c==')') cnt--;
+        }
+        return ans;
+    }
+};
