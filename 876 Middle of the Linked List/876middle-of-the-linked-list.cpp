be4d955208579a318e1a0af15bcc2b4d0c78d47e
Dhruv Kumar Gupta <dhruvkumargupta@Dhruvs-MacBook-Air-M4.local>
2026-05-18 14:22:44 +0000

    Code challenge submission for question: 876 Middle of the Linked List

---
+++ 876 Middle of the Linked List/876middle-of-the-linked-list.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int cnt=0;
+        ListNode* temp = head;
+        while(temp!=nullptr)
+        {
+            cnt++;
+            temp=temp->next;
+
+        }
+        int k = ceil((cnt+1)/2.0);
+        cnt=1;
+        temp=head;
+        while(cnt!=k)
+        {
+            temp=temp->next;
+            cnt++;
+        }
+        return temp;
+
+    }
+};
