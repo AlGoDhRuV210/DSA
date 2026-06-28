3d96ee67af349abf465d6ce755290f561dafe570
Dhruv Kumar Gupta <dhruvkumargupta@Dhruvs-MacBook-Air-M4.local>
2026-06-28 18:50:05 +0000

    Code challenge submission for question: 199 Binary Tree Right Side View

---
+++ 199 Binary Tree Right Side View/199binary-tree-right-side-view.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> v;
+        if (root == nullptr)
+            return v;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int s = q.size();
+            vector<int> level;
+            for (int i = 0; i < s; i++) {
+                TreeNode* top = q.front();
+                q.pop();
+                level.push_back(top->val);
+                if (top->left)
+                    q.push(top->left);
+                if (top->right)
+                    q.push(top->right);
+            }
+            v.push_back(level);
+        }
+        return v;
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> v;
+        if (!root)
+            return {};
+        vector<vector<int>> temp = levelOrder(root);
+        for (auto i : temp) {
+            v.push_back(i[i.size() - 1]);
+        }
+        return v;
+    }
+};
