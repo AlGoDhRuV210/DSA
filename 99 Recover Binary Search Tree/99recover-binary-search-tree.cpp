47e4b58352a6d4b96c5b9ad468dc02ed6b3e6e31
Dhruv Kumar Gupta <dhruvkumargupta@Dhruvs-MacBook-Air-M4.local>
2026-07-09 13:54:56 +0000

    Code challenge submission for question: 99 Recover Binary Search Tree

---
+++ 99 Recover Binary Search Tree/99recover-binary-search-tree.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* first;
+    TreeNode* middle;
+    TreeNode* last;
+    TreeNode* prev;
+
+    void inorder(TreeNode* root) {
+        if (!root)
+            return;
+        inorder(root->left);
+
+        if (prev && (root->val < prev->val))
+         {
+            if (!first) {
+                first = prev;
+                middle = root;
+            } else {
+                last = root;
+            }
+        }
+        prev = root;
+        root = root->right;
+        inorder(root);
+    }
+    void recoverTree(TreeNode* root) {
+        first=middle=last=nullptr;
+        prev=new TreeNode(INT_MIN);
+        inorder(root);
+        if (first && last) {
+            swap(first->val, last->val);
+        } else if(first && middle) {
+            swap(first->val, middle->val);
+        }
+    }
+};
