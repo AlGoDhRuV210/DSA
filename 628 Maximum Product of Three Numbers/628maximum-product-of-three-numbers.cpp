8feb723fb4b5547db0f9d7b8cf0da796f3792b8f
Dhruv Kumar Gupta <dhruvkumargupta@Dhruvs-MacBook-Air-M4.local>
2026-07-26 18:50:48 +0000

    Code challenge submission for question: 628 Maximum Product of Three Numbers

---
+++ 628 Maximum Product of Three Numbers/628maximum-product-of-three-numbers.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        if (nums[n - 1] == 0)
+            return 0;
+        if (nums[n - 1] < 0)
+            return nums[n - 1] * nums[n - 2] * nums[n - 3];
+
+        return max(nums[n - 1] * nums[n - 2] * nums[n - 3],
+                   nums[n - 1] * nums[0] * nums[1]);
+    }
+};
