4352f2d45442bfcc5b0962937dc162fea18376bc
Dhruv Kumar Gupta <dhruvkumargupta@Dhruvs-MacBook-Air-M4.local>
2026-08-02 16:26:39 +0000

    Code challenge submission for question: 309 Best Time to Buy and Sell Stock with Cooldown

---
+++ 309 Best Time to Buy and Sell Stock with Cooldown/309best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int n = prices.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+        // dp[index][buy][no of trades left]
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            /* code */
+            dp[i][1] = max(dp[i + 1][1], -prices[i] + dp[i + 1][0]);
+            int m = i + 2 <= n  ? dp[i + 2][1] : 0;
+            dp[i][0] = max(dp[i + 1][0], prices[i] + m);
+        }
+
+        return dp[0][1];
+    }
+};
