1397aec31fbdbd5123db1b6c7ea4cb9560ac1924
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:55:48 +0530

    Add Dolce Vita [C] from Codeforces

---
+++ 1671/C - Dolce Vita/solution.cpp
@@ -0,0 +1,74 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+using namespace std;
+ 
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define vi vector<long long>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define mii map<int, int>
+#define mci map<char, int>
+#define umci unordered_map<char, int>
+#define umii unordered_map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define sort(x) sort(x.begin(), x.end())
+#define int long long
+ 
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{
+    int n, x;
+    cin >> n >> x;
+    vi a(n + 1);
+    f(i, n) cin >> a[i + 1];
+    sort(a);
+    int ans = 0;
+    int pre = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        ans += max(0LL, (x - pre - a[i] + i) / i);
+        pre += a[i];
+    }
+    if (a[0] <= x)
+        cout << ans << endl;
+    else
+        cout << 0 << endl;
+}
+ 
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
