f261d5e413318e96f5235235adc55775427d32de
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:21:14 +0530

    Add Ambitious Kid [A] from Codeforces

---
+++ 1866/A - Ambitious Kid/solution.cpp
@@ -0,0 +1,73 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i,n) for (int i = 0; i < n; i++)
+#define f1(i,a,b) for (int i = a; i <= b; i++)
+#define f2(i,a,b,c) for (int i = a; i <= b; i += c)
+#define vi vector<long long>
+#define vs vector<string>
+#define pii pair<int, int>
+#define vpii vector<PII>
+#define mii map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define asort(x) sort(all(x))
+#define int long long
+ 
+ 
+ 
+ 
+#define Its ios::sync_with_stdio(false);
+#define Dhruv cin.tie(nullptr);
+#define here cout.tie(nullptr);
+ 
+using namespace std;
+ 
+ 
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{   int n ;
+    cin>>n;
+    vi v(n); f(i,n) cin>>v[i];
+    int ans =INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        ans=min(ans,abs(v[i]));
+    }
+    cout<<ans<<endl;
+ 
+    
+}
+ 
+ 
+int32_t main() {
+    Its Dhruv here
+    int t=1;
+    
+    while (t--) {
+       solve();
+    }
+ return 0; 
+}
