bfbb2ef51e298f380fc9e8bf9f30766a5ada4621
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-14 00:03:11 +0530

    Add Blocked [A] from Codeforces

---
+++ 2220/A - Blocked/solution.cpp
@@ -0,0 +1,105 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+using namespace std;
+ 
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define vi vector<long long>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define mii map<int, int>
+#define mci map<char, int>
+#define umci unordered_map<char, int>
+#define umii unordered_map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rev(x) reverse(all(x))
+#define asort(x) sort(all(x))
+#define dsort(x) sort(all(x), greater<>())
+#define int long long
+#define double long double
+#define endl '
+'
+ 
+long long gcd(long long a, long long b)
+{
+    while (b)
+    {
+        long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+long long lcm(long long a, long long b)
+{
+    return (a / gcd(a, b)) * b;
+}
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    vi a(n);
+    f(i, n) cin >> a[i];
+ 
+    dsort(a);
+    int pre = a[0];
+    set<int> s;
+    bool ok = true;
+    s.insert(a[0]);
+    for (int i = 1; i < n; i++)
+    {
+        s.insert(a[i]);
+        if (a[i] == pre)
+        {
+            ok = false;
+            break;
+        }
+        pre += a[i];
+    }
+    if (s.size() != n)
+        ok = false;
+    if (ok)
+    {
+        for (auto i : a)
+            cout << i << " ";
+        cout << endl;
+    }
+    else
+        cout << -1 << endl;
+}
+ 
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
