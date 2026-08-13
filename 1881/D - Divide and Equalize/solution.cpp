dcdfea13eb096d071a365b8d787dcd1bb3acc78b
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:58:48 +0530

    Add Divide and Equalize [D] from Codeforces

---
+++ 1881/D - Divide and Equalize/solution.cpp
@@ -0,0 +1,110 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+using namespace std;
+ 
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define vi vector<long long>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define mii map<int, int>
+#define mci map<char, int>
+#define umci unordered_map<char, int>
+#define umii unordered_map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rev(x) reverse(all(x))
+#define asort(x) sort(all(x))
+#define dsort(x) sort(all(x), greater<>())
+#define int long long
+#define double long double
+#define endl '
+'
+ 
+long long gcd(long long a, long long b)
+{
+    while (b)
+    {
+        long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+long long lcm(long long a, long long b)
+{
+    return (a / gcd(a, b)) * b;
+}
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    vi a(n);
+    f(i, n) cin >> a[i];
+    mii mpp;
+    for (int i = 0; i < n; i++)
+    {
+        int ele = a[i];
+        for (int j = 2; j * j <= ele; j++)
+        {
+            if (ele % j == 0)
+            {
+                mpp[j]++;
+                ele = ele / j;
+                while (ele % j == 0)
+                {
+                    mpp[j]++;
+                    ele = ele / j;
+                }
+            }
+        }
+        if (ele > 1)
+            mpp[ele]++;
+    }
+    for (auto i : mpp)
+    {
+        if ((i.second % n) != 0)
+        {
+            cout << "NO" << endl;
+            return;
+        }
+    }
+    cout << "YES" << endl;
+}
+ 
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
