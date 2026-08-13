f95884770e7e98765200d949d8286d3b713a601c
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:34:05 +0530

    Add 2D Traveling [B] from Codeforces

---
+++ 1869/B - 2D Traveling/solution.cpp
@@ -0,0 +1,98 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define f1(i, a, b) for (int i = a; i <= b; i++)
+#define f2(i, a, b, c) for (int i = a; i <= b; i += c)
+#define vi vector<long long>
+#define vs vector<string>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define mii map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define sort(x) sort(x.begin(), x.end())
+#define int long long
+ 
+#define Its ios::sync_with_stdio(false);
+#define Dhruv cin.tie(nullptr);
+#define here cout.tie(nullptr);
+ 
+using namespace std;
+ 
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+int cost(pii a, pii b)
+{
+    return abs(a.first - b.first) + abs(b.second - a.second);
+}
+void solve()
+{
+    int n, k, a, b;
+    cin >> n >> k >> a >> b;
+    vpii major;
+ 
+    vpii v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i].first;
+        cin >> v[i].second;
+    }
+    for (int i = 0; i < k; i++)
+    {
+        major.push_back(v[i]);
+    }
+ 
+    int ans=0;
+    pii start = v[a - 1];
+    pii end = v[b - 1];
+    ans = cost(start, end);
+ 
+    if (k == 0)
+    {
+        cout << ans << endl;
+        return;
+    }
+    int da = LLONG_MAX;
+    int db = LLONG_MAX;
+    for (int i = 0; i < k; i++)
+    {
+        da=min(da,cost(start,major[i]));
+        db=min(db,cost(end,major[i]));
+          
+    }
+ 
+    ans=min(ans,da+db);
+    cout<<ans<<endl;
+}
+ 
+int32_t main()
+{
+    Its Dhruv here int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
