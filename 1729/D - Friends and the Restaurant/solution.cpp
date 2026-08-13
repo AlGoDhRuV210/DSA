0b1e8b28a0c062fff306ebf13100188e163932b6
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:54:57 +0530

    Add Friends and the Restaurant [D] from Codeforces

---
+++ 1729/D - Friends and the Restaurant/solution.cpp
@@ -0,0 +1,93 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+using namespace std;
+ 
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define vi vector<long long>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define mii map<int, int>
+#define mci map<char, int>
+#define umci unordered_map<char, int>
+#define umii unordered_map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define sort(x) sort(x.begin(), x.end())
+#define int long long
+ 
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    vi a(n), b(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+    int ans = 0;
+    vi v(n);
+    for (int i = 0; i < n; i++)
+    {
+        v[i] = b[i] - a[i];
+    }
+    sort(v);
+    reverse(all(v));
+    int i = 0;
+    int j = n - 1;
+    while (i < j)
+    {
+        if (v[i] + v[j] >= 0)
+        {
+            ans++;
+            i++;
+            j--;
+        }
+        else
+        {
+            j--;
+        }
+    }
+    cout << ans << endl;
+}
+ 
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
