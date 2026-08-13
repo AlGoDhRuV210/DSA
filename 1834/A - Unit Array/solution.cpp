282ea1715ad5bb4ffacac131868155d1429ff3cb
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:22:13 +0530

    Add Unit Array [A] from Codeforces

---
+++ 1834/A - Unit Array/solution.cpp
@@ -0,0 +1,84 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define f1(i, a, b) for (int i = a; i <= b; i++)
+#define f2(i, a, b, c) for (int i = a; i <= b; i += c)
+#define vi vector<long long>
+#define vs vector<string>
+#define pii pair<int, int>
+#define vpii vector<PII>
+#define mii map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define asort(x) sort(all(x))
+#define int long long
+ 
+#define Its ios::sync_with_stdio(false);
+#define Dhruv cin.tie(nullptr);
+#define here cout.tie(nullptr);
+ 
+using namespace std;
+ 
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    int a = 0, b = 0; // a 1 b -1
+    int ans = 0;
+    vi v(n);
+    f(i, n)
+    {
+        cin >> v[i];
+        if (v[i] == 1)
+            a++;
+        else
+            b++;
+    }
+    if (a >= b)
+    {
+        if (b % 2)
+            ans = 1;
+    }
+    else
+    {
+        ans += (b -a+1) / 2;
+        if ((b - ans) % 2)
+            ans++;
+    }
+    cout << ans << endl;
+}
+ 
+int32_t main()
+{
+    Its Dhruv here int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
