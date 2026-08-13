d2aa815efa1affda26c393b18af1d3b57d442164
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:25:47 +0530

    Add Array Cloning Technique [B] from Codeforces

---
+++ 1665/B - Array Cloning Technique/solution.cpp
@@ -0,0 +1,102 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define f1(i, a, b) for (int i = a; i <= b; i++)
+#define f2(i, a, b, c) for (int i = a; i <= b; i += c)
+#define vi vector<long long>
+#define vs vector<string>
+#define pii pair<int, int>
+#define vpii vector<PII>
+#define mii map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define asort(x) sort(all(x))
+#define int long long
+ 
+#define Its ios::sync_with_stdio(false);
+#define Dhruv cin.tie(nullptr);
+#define here cout.tie(nullptr);
+ 
+using namespace std;
+ 
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    vi a(n);
+    f(i, n) cin >> a[i];
+    sort(a.begin(), a.end());
+    int mx = 0;
+    int cnt = 1;
+    int c = a[0];
+    int ans = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] == c)
+            cnt++;
+        else
+        {
+            mx = max(mx, cnt);
+            cnt = 1;
+            c=a[i];
+        }
+    }
+    mx = max(mx, cnt);
+    if (mx == n)
+        ans = 0;
+    else
+    {
+        int rem = n - mx;
+        if (rem <= mx)
+            ans = ans + 1 + rem;
+        else
+        {
+            ans++;
+            while (rem > mx)
+            {
+ 
+                ans += mx;
+                rem = rem - mx;
+                mx = mx * 2;
+                ans++;
+            }
+            ans += rem;
+        }
+    }
+    cout << ans << endl;
+}
+ 
+int32_t main()
+{
+    Its Dhruv here int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
