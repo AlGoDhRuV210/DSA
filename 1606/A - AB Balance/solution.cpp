622d76ccf6b1b37cafd86f9506a16a852f7d80d0
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:25:59 +0530

    Add AB Balance [A] from Codeforces

---
+++ 1606/A - AB Balance/solution.cpp
@@ -0,0 +1,82 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define f1(i, a, b) for (int i = a; i <= b; i++)
+#define f2(i, a, b, c) for (int i = a; i <= b; i += c)
+#define vi vector<long long>
+#define vs vector<string>
+#define pii pair<int, int>
+#define vpii vector<PII>
+#define mii map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define asort(x) sort(all(x))
+#define int long long
+ 
+#define Its ios::sync_with_stdio(false);
+#define Dhruv cin.tie(nullptr);
+#define here cout.tie(nullptr);
+ 
+using namespace std;
+ 
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{
+    string s;
+    cin >> s;
+    int cnt = 0;
+    char c = s[0];
+    if(s.length()==1){ cout<<s<<endl; return;}
+    for (int i = 1; i < s.length(); i++)
+    {
+        if (s[i] != c)
+        {
+            cnt++;
+            c = s[i];
+        }
+    }
+    if (cnt % 2 == 0)
+        cout << s << endl;
+    else
+    {
+        if(s[0]=='a')
+        cout << s.substr(0, s.length() - 1) +"a"<<endl;
+        else 
+        cout << s.substr(0, s.length() - 1) +"b"<<endl;
+ 
+    }
+}
+ 
+int32_t main()
+{
+    Its Dhruv here int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
