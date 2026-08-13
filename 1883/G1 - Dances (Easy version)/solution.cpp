8d5ca368dead2a63f0049503a9ea2a18bb9c0381
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-14 00:01:57 +0530

    Add Dances (Easy version) [G1] from Codeforces

---
+++ 1883/G1 - Dances (Easy version)/solution.cpp
@@ -0,0 +1,122 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+using namespace std;
+ 
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define vi vector<long long>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define mii map<int, int>
+#define mci map<char, int>
+#define umci unordered_map<char, int>
+#define umii unordered_map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rev(x) reverse(all(x))
+#define asort(x) sort(all(x))
+#define dsort(x) sort(all(x), greater<>())
+#define int long long
+#define double long double
+#define endl '
+'
+ 
+long long gcd(long long a, long long b)
+{
+    while (b)
+    {
+        long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+long long lcm(long long a, long long b)
+{
+    return (a / gcd(a, b)) * b;
+}
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+bool check(int k, vi a, vi b)
+{
+    for (int i = 0; i < k; i++)
+    {
+        a.pop_back();
+        b.pop_back();
+    }
+ 
+    rev(b);
+    for (int i = 0; i < a.size(); i++)
+    {
+        if (a[i] >= b[i])
+            return false;
+    }
+    return true;
+}
+ 
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vi a(n), b(n);
+    a[0] = 1;
+    for (int i = 1; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+    asort(a);
+    dsort(b);
+    int l = 0;
+    int h = n;
+    int ans = h;
+    while (l <= h)
+    {
+        int mid = l + (h - l) / 2;
+        if (check(mid, a, b))
+        {
+            ans = mid;
+            h = mid - 1;
+        }
+        else
+            l = mid + 1;
+    }
+ 
+    cout << ans << endl;
+}
+ 
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
