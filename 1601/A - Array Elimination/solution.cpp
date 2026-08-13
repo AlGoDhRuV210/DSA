31d09109b38fa3509bac46e00f6adfd8392b57bb
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:59:29 +0530

    Add Array Elimination [A] from Codeforces

---
+++ 1601/A - Array Elimination/solution.cpp
@@ -0,0 +1,110 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+using namespace std;
+ 
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define vi vector<long long>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define mii map<int, int>
+#define mci map<char, int>
+#define umci unordered_map<char, int>
+#define umii unordered_map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rev(x) reverse(all(x))
+#define asort(x) sort(all(x))
+#define dsort(x) sort(all(x), greater<>())
+#define int long long
+#define double long double
+#define endl '
+'
+ 
+long long gcd(long long a, long long b)
+{
+    while (b)
+    {
+        long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+long long lcm(long long a, long long b)
+{
+    return (a / gcd(a, b)) * b;
+}
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    vi a(n);
+    f(i, n) cin >> a[i];
+    vi v(31, 0);
+    for (int i = 0; i < n; i++)
+    {
+ 
+        int ele = a[i];
+        int index = 0;
+        while (ele > 0)
+        {
+            if (ele & 1)
+                v[index]++;
+            ele = ele >> 1;
+            index++;
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        int flag = 1;
+        for (int j = 0; j < 31; j++)
+        {
+            if (v[j] % i != 0)
+            {
+                flag = 0;
+                break;
+            }
+        }
+        if (flag)
+            cout << i << " ";
+    }
+    cout << endl;
+}
+ 
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
