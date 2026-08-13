122c39205d8e62487b9420e7acd99fd6365ff8f8
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:20:24 +0530

    Add Cover in Water [A] from Codeforces

---
+++ 1900/A - Cover in Water/solution.cpp
@@ -0,0 +1,87 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i,n) for (int i = 0; i < n; i++)
+#define f1(i,a,b) for (int i = a; i <= b; i++)
+#define f2(i,a,b,c) for (int i = a; i <= b; i += c)
+#define vi vector<long long>
+#define vs vector<string>
+#define pii pair<int, int>
+#define vpii vector<PII>
+#define mii map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define asort(x) sort(all(x))
+#define int long long
+ 
+ 
+ 
+ 
+#define Its ios::sync_with_stdio(false);
+#define Dhruv cin.tie(nullptr);
+#define here cout.tie(nullptr);
+ 
+using namespace std;
+ 
+ 
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{   int n ;
+    cin>>n;
+    string s  ; cin>>s;
+    int dot=0;
+    for(auto c : s) {if(c=='.') dot++;}
+    bool flag=false;
+    for (int i = 0; i < n; i++)
+    {
+        if(s[i]=='.')
+        {
+            int cnt=1;
+            i++;
+            while(i<n && s[i]=='.')
+            {
+                cnt++;
+                if(cnt>=3) {flag=true; break;}
+                i++;
+            }
+            if(flag) break;
+            i--;
+        }
+    }
+    if(flag) cout<<'2'<<endl;
+    else cout<<dot<<endl;
+ 
+}
+ 
+ 
+int32_t main() {
+    Its Dhruv here
+    int t;
+    cin >> t;
+    while (t--) {
+       solve();
+    }
+ return 0; 
+}
