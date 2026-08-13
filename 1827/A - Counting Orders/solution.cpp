adfdfa11a41665d98853b56a708242007cb123cd
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:34:40 +0530

    Add Counting Orders [A] from Codeforces

---
+++ 1827/A - Counting Orders/solution.cpp
@@ -0,0 +1,84 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define f1(i, a, b) for (int i = a; i <= b; i++)
+#define f2(i, a, b, c) for (int i = a; i <= b; i += c)
+#define vi vector<long long>
+#define vs vector<string>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define mii map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define sort(x) sort(x.begin(), x.end())
+#define int long long
+ 
+#define Its ios::sync_with_stdio(false);
+#define Dhruv cin.tie(nullptr);
+#define here cout.tie(nullptr);
+ 
+using namespace std;
+ 
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    vi a(n), b(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n; i++)
+        cin >> b[i];
+    int sum = 1;
+    sort(a);
+    sort(b);
+    int minus = 0;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int k = upper_bound(a.begin(), a.end(), b[i]) - a.begin();
+        k = n - k - minus;
+        sum = (sum * k);
+        if (sum <= 0)
+        {
+            sum = 0;
+            break;
+        }
+        minus++;
+        sum %= MOD1;
+    }
+    cout<<sum<<endl;
+}
+ 
+int32_t main()
+{
+    Its Dhruv here int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
