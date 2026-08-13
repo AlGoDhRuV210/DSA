ededb0f59c918b44d48bc22371a756ebed254620
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:27:57 +0530

    Add Monsters [B] from Codeforces

---
+++ 1849/B - Monsters/solution.cpp
@@ -0,0 +1,91 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define f1(i, a, b) for (int i = a; i <= b; i++)
+#define f2(i, a, b, c) for (int i = a; i <= b; i += c)
+#define vi vector<long long>
+#define vs vector<string>
+#define pii pair<int, int>
+#define vpii vector<PII>
+#define mii map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define asort(x) sort(all(x))
+#define int long long
+ 
+#define Its ios::sync_with_stdio(false);
+#define Dhruv cin.tie(nullptr);
+#define here cout.tie(nullptr);
+ 
+using namespace std;
+ 
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+bool comp(pair<int, int> p1, pair<int, int> p2)
+{
+    if (p1.first > p2.first)
+        return true;
+    else if(p1.first < p2.first) return false;
+    else
+    {
+        if(p1.second>p2.second) return false;
+        else return true;
+    }
+}
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    vi a(n);
+    f(i, n) cin >> a[i];
+    vector<pair<int, int>> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        v[i] = {a[i], i + 1};
+    }
+    for (int i = 0; i < n; i++)
+    {
+        v[i].first %= k;
+        if (v[i].first == 0)
+            v[i].first = k;
+    }
+    sort(v.begin(), v.end(), comp);
+ 
+    for (auto i : v)
+        cout << i.second << " ";
+    cout << endl;
+ 
+}
+ 
+int32_t main()
+{
+    Its Dhruv here int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
