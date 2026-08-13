71944478a4472e473a90e1a6463ed16aa0fd2bd3
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:23:30 +0530

    Add Forked! [A] from Codeforces

---
+++ 1904/A - Forked!/solution.cpp
@@ -0,0 +1,95 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i,n) for (int i = 0; i < n; i++)
+#define f1(i,a,b) for (int i = a; i <= b; i++)
+#define f2(i,a,b,c) for (int i = a; i <= b; i += c)
+#define vi vector<long long>
+#define vs vector<string>
+#define pii pair<int, int>
+#define vpii vector<PII>
+#define mii map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define asort(x) sort(all(x))
+#define int long long
+ 
+ 
+ 
+ 
+#define Its ios::sync_with_stdio(false);
+#define Dhruv cin.tie(nullptr);
+#define here cout.tie(nullptr);
+ 
+using namespace std;
+ 
+ 
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{   int a,b,xk,yk,xq,yq;
+ 
+    cin>>a>>b;
+    cin>>xk>>yk;
+    cin>>xq>>yq;
+    set<pair<int,int>> s1,s2;
+    //king
+ 
+        s1.insert({xk+a,yk+b});
+        s1.insert({xk-a,yk+b});
+        s1.insert({xk+a,yk-b});
+        s1.insert({xk-a,yk-b});
+        s1.insert({xk+b,yk+a});
+        s1.insert({xk-b,yk+a});
+        s1.insert({xk+b,yk-a});
+        s1.insert({xk-b,yk-a});
+ 
+        s2.insert({xq+a,yq+b});
+        s2.insert({xq-a,yq+b});
+        s2.insert({xq+a,yq-b});
+        s2.insert({xq-a,yq-b});
+        s2.insert({xq+b,yq+a});
+        s2.insert({xq-b,yq+a});
+        s2.insert({xq+b,yq-a});
+        s2.insert({xq-b,yq-a});
+    int ans=0;
+    for( auto i : s1)
+    {
+        if(s2.find(i)!=s2.end()) ans++;
+    }
+    cout<<ans<<endl;
+ 
+    
+}
+ 
+ 
+int32_t main() {
+    Its Dhruv here
+    int t;
+    cin >> t;
+    while (t--) {
+       solve();
+    }
+ return 0; 
+}
