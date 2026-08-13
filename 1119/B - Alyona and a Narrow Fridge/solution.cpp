67972333e779d6d93e661ffc55872b8c9d397428
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-14 00:00:48 +0530

    Add Alyona and a Narrow Fridge [B] from Codeforces

---
+++ 1119/B - Alyona and a Narrow Fridge/solution.cpp
@@ -0,0 +1,105 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+using namespace std;
+ 
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define vi vector<long long>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define mii map<int, int>
+#define mci map<char, int>
+#define umci unordered_map<char, int>
+#define umii unordered_map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rev(x) reverse(all(x))
+#define asort(x) sort(all(x))
+#define dsort(x) sort(all(x), greater<>())
+#define int long long
+#define double long double
+#define endl '
+'
+ 
+long long gcd(long long a, long long b)
+{
+    while (b)
+    {
+        long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+long long lcm(long long a, long long b)
+{
+    return (a / gcd(a, b)) * b;
+}
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{
+    int n, h;
+    cin >> n >> h;
+    vi a(n);
+    f(i, n) cin >> a[i];
+    int low = 1;
+    int high = n;
+    int ans = 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        vi v(a.begin(), a.begin() + mid);
+        asort(v);
+        int j = v.size() - 1;
+        int height = 0;
+        while (j >= 0)
+        {
+            height += v[j];
+            j -= 2;
+            if (height > h)
+                break;
+        }
+        if (height > h)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            ans = mid;
+            low = mid + 1;
+        }
+    }
+    cout << ans << endl;
+}
+ 
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+     
+        solve();
+    return 0;
+}
