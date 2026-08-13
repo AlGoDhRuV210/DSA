634738814310e18d5c27283c05d57439e854b69d
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:30:35 +0530

    Add Euclid, Sequence and Two Numbers [A] from Codeforces

---
+++ 2234/A - Euclid, Sequence and Two Numbers/solution.cpp
@@ -0,0 +1,76 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define f1(i, a, b) for (int i = a; i <= b; i++)
+#define f2(i, a, b, c) for (int i = a; i <= b; i += c)
+#define vi vector<long long>
+#define vs vector<string>
+#define pii pair<int, int>
+#define vpii vector<PII>
+#define mii map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define asort(x) sort(all(x))
+#define int long long
+ 
+#define Its ios::sync_with_stdio(false);
+#define Dhruv cin.tie(nullptr);
+#define here cout.tie(nullptr);
+ 
+using namespace std;
+ 
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    vi v(n);
+    f(i, n) cin >> v[i];
+    int flag = 1;
+    sort(v.begin(), v.end());
+    reverse(v.begin(), v.end());
+    for (int i = 0; i < n - 2; i++)
+    {
+        if ((v[i] % v[i + 1]) !=( v[i + 2]))
+        {
+            flag = 0;
+            break;
+        }
+    }
+    if(flag==0) cout<<-1<<endl;
+    else cout<<v[0]<<" "<<v[1]<<endl;
+}
+ 
+int32_t main()
+{
+    Its Dhruv here int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
