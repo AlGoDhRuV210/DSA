7a15e2b47d0fb0af3178dfd172450839eb4b167e
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-14 00:00:28 +0530

    Add Buying Shovels [D] from Codeforces

---
+++ 1360/D - Buying Shovels/solution.cpp
@@ -0,0 +1,98 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+using namespace std;
+ 
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define vi vector<long long>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define mii map<int, int>
+#define mci map<char, int>
+#define umci unordered_map<char, int>
+#define umii unordered_map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rev(x) reverse(all(x))
+#define asort(x) sort(all(x))
+#define dsort(x) sort(all(x), greater<>())
+#define int long long
+#define double long double
+#define endl '
+'
+ 
+long long gcd(long long a, long long b)
+{
+    while (b)
+    {
+        long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+long long lcm(long long a, long long b)
+{
+    return (a / gcd(a, b)) * b;
+}
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    vi v;
+    for (int i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            v.push_back(i);
+            if (n / i != i)
+                v.push_back(n/i);
+        }
+    }
+    asort(v);
+    for (int i = v.size() - 1; i >= 0; i--)
+    {
+        if (v[i] <= k)
+        {
+            cout << n / v[i] << endl;
+            return;
+        }
+    }
+}
+ 
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
