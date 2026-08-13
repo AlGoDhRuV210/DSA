728b2a4a2a96bc56173155044518336a974cd451
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:20:53 +0530

    Add Doremy's Paint 3 [A] from Codeforces

---
+++ 1890/A - Doremy's Paint 3/solution.cpp
@@ -0,0 +1,83 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define f1(i, a, b) for (int i = a; i <= b; i++)
+#define f2(i, a, b, c) for (int i = a; i <= b; i += c)
+#define vi vector<long long>
+#define vs vector<string>
+#define pii pair<int, int>
+#define vpii vector<PII>
+#define mii map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define asort(x) sort(all(x))
+#define int long long
+ 
+#define Its ios::sync_with_stdio(false);
+#define Dhruv cin.tie(nullptr);
+#define here cout.tie(nullptr);
+ 
+using namespace std;
+ 
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    unordered_map<int, int> mpp;
+    vi v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+        mpp[v[i]]++;
+    }
+    if (mpp.size() > 2)
+        cout << "NO" << endl;
+    else if (mpp.size()==1)
+        cout << "YES" << endl;
+    else
+    {
+        int f =( *mpp.begin()).second;
+        int s =( *(++mpp.begin())).second;
+       
+       
+        if (abs(f-s) <= 1)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+}
+ 
+int32_t main()
+{
+    Its Dhruv here int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
