efde089742bb50057f47c9ee4e11158c86c6dded
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:56:18 +0530

    Add Make Them Equal [C] from Codeforces

---
+++ 1594/C - Make Them Equal/solution.cpp
@@ -0,0 +1,98 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+using namespace std;
+ 
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define vi vector<long long>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define mii map<int, int>
+#define mci map<char, int>
+#define umci unordered_map<char, int>
+#define umii unordered_map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define sort(x) sort(x.begin(), x.end())
+#define int long long
+ 
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    char c;
+    cin >> c;
+    string s;
+    cin >> s;
+    int flag = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] != c)
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if (!flag)
+    {
+        cout << 0 << endl;
+        return;
+    }
+    if (s[n - 1] == c)
+    {
+        cout << 1 << endl;
+        cout << n << endl;
+        return;
+    }
+ 
+    for (int i = n - 2; i >= (n / 2); i--)
+    {
+        if (s[i] == c)
+        {
+ 
+            cout << 1 << endl;
+            cout << i + 1 << endl;
+            return;
+        }
+    }
+ 
+    cout << 2 << endl
+         << n - 1 << " " << n << endl;
+}
+ 
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
