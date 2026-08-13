08fb13e93d8bae984af51e0836d21702873279d5
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:24:50 +0530

    Add Mainak and Array [A] from Codeforces

---
+++ 1726/A - Mainak and Array/solution.cpp
@@ -0,0 +1,80 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i,n) for (int i = 0; i < n; i++)
+#define f1(i,a,b) for (int i = a; i <= b; i++)
+#define f2(i,a,b,c) for (int i = a; i <= b; i += c)
+#define vi vector<long long>
+#define vs vector<string>
+#define pii pair<int, int>
+#define vpii vector<PII>
+#define mii map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define asort(x) sort(all(x))
+#define int long long
+ 
+ 
+ 
+ 
+#define Its ios::sync_with_stdio(false);
+#define Dhruv cin.tie(nullptr);
+#define here cout.tie(nullptr);
+ 
+using namespace std;
+ 
+ 
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{   int n ;
+    cin>>n;
+    vi v(n);
+    for (int i= 0; i < n; i++)
+    {
+        cin>>v[i];
+    }
+    int ans=INT_MIN;
+    for (int i= 0; i < n-1; i++)
+    {
+        ans= max(ans,v[i]-v[i+1]);
+    }
+    int mx=*max_element(v.begin(),v.end());
+    int mn=*min_element(v.begin(),v.end());
+    ans=max(ans,mx-v[0]);
+    ans=max(ans,v[n-1]-mn);
+    cout<<ans<<endl;
+    
+}
+ 
+ 
+int32_t main() {
+    Its Dhruv here
+    int t;
+    cin >> t;
+    while (t--) {
+       solve();
+    }
+ return 0; 
+}
