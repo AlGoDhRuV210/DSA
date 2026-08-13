8267ca0681d26a452e617290f462bc0503c3c11e
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:32:32 +0530

    Add Absolute Cinema [B] from Codeforces

---
+++ 2229/B - Absolute Cinema/solution.cpp
@@ -0,0 +1,83 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i,n) for (int i = 0; i < n; i++)
+#define f1(i,a,b) for (int i = a; i <= b; i++)
+#define f2(i,a,b,c) for (int i = a; i <= b; i += c)
+#define vi vector<long long>
+#define vs vector<string>
+#define pii pair<int, int>
+#define vpii vector<pair<int,int>>
+#define mii map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define sort(x) sort(x.begin(),x.end())
+#define int long long
+ 
+ 
+ 
+ 
+#define Its ios::sync_with_stdio(false);
+#define Dhruv cin.tie(nullptr);
+#define here cout.tie(nullptr);
+ 
+using namespace std;
+ 
+ 
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{   int n ;
+    cin>>n;
+    vi a(n),b(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin>>a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin>>b[i];
+    }
+ 
+ 
+for (int i = 0; i < n; i++)
+{
+    if(a[i]>b[i]) swap(a[i],b[i]);
+}
+int ans = (*max_element(all(a)) ) +  accumulate(all(b),0LL);
+ 
+    cout<<ans<<endl;
+    
+}
+ 
+ 
+int32_t main() {
+    Its Dhruv here
+    int t;
+    cin >> t;
+    while (t--) {
+       solve();
+    }
+ return 0; 
+}
