80cad3fb558365a25f3d8331e2634db6c2bfa6a9
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:17:03 +0530

    Add Array Coloring [A] from Codeforces

---
+++ 2191/A - Array Coloring/solution.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <math.h>
+ 
+using namespace std;
+ 
+/* Fast I/O */
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+ 
+/* Shortcuts */
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+ 
+/* Loops */
+#define fori(i, a, b) for (int i = a; i < b; i++)
+#define ford(i, a, b) for (int i = a; i >= b; i--)
+ 
+/* Constants */
+const int MOD = 1000000007;
+const long long INF = 1000000000000000000LL;
+ 
+/* Debug */
+#define debug(x) cerr << #x << " = " << x << endl;
+ 
+int main()
+{
+    fastio();
+ 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        bool ans = true;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 2; j < n; j += 2)
+            {
+                if (arr[j] - arr[i] == 1 || arr[j] - arr[i] == -1)
+                {
+                    ans = false;
+                    cout << "NO" << endl;
+                    break;
+                }
+            }
+            if (ans == false)
+                break;
+        }
+        if(ans==true) cout<<"Yes"<<endl;
+    }
+    return 0;
+}
