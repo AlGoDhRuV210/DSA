22aeea867e3b345e01669c5984856c444318b7df
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:27:11 +0530

    Add 01 Game [B] from Codeforces

---
+++ 1373/B - 01 Game/solution.cpp
@@ -0,0 +1,74 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define f1(i, a, b) for (int i = a; i <= b; i++)
+#define f2(i, a, b, c) for (int i = a; i <= b; i += c)
+#define vi vector<long long>
+#define vs vector<string>
+#define pii pair<int, int>
+#define vpii vector<PII>
+#define mii map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define asort(x) sort(all(x))
+#define int long long
+ 
+#define Its ios::sync_with_stdio(false);
+#define Dhruv cin.tie(nullptr);
+#define here cout.tie(nullptr);
+ 
+using namespace std;
+ 
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{
+    string s;
+    cin >> s;
+    char c = s[0];
+    int one = 0;
+    int zero = 0;
+    for(auto i : s)
+    if (i == '0')
+        zero++;
+    else
+        one++;
+   int ans = min(zero,one);
+   if(ans%2) cout<<"DA"<<endl; else cout<<"NET"<<endl;
+ 
+   
+ }
+    
+ 
+int32_t main()
+{
+    Its Dhruv here int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
