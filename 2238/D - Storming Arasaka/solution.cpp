e89c2490bd5f010796359ee9c0560c99465090a0
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:56:01 +0530

    Add Storming Arasaka [D] from Codeforces

---
+++ 2238/D - Storming Arasaka/solution.cpp
@@ -0,0 +1,88 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+using namespace std;
+ 
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define vi vector<long long>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define mii map<int, int>
+#define mci map<char, int>
+#define umci unordered_map<char, int>
+#define umii unordered_map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define sort(x) sort(x.begin(), x.end())
+#define int long long
+ 
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+int gcd(int a, int b)
+{
+   if (b == 0)
+      return a;
+   return gcd(b, a % b);
+}
+void solve()
+{
+   int n;
+   cin >> n;
+   int sumExp = 0;
+   int distinct = 0;
+   for (long long p = 2; p * p <= n; p++)
+   {
+      if (n % p == 0)
+      {
+         distinct++;
+         while (n % p == 0)
+         {
+            sumExp++;
+            n /= p;
+         }
+      }
+   }
+   if (n > 1)
+   {
+      distinct++;
+      sumExp++;
+   }
+   cout << sumExp - 1 + distinct << '
+';
+}
+ 
+int32_t main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+ 
+   int t;
+   cin >> t;
+   while (t--)
+   {
+      solve();
+   }
+ 
+   return 0;
+}
