ea17ca82fa91a6fbf7c6ab275c32eae2688bcf0f
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:32:05 +0530

    Add Johnny and Ancient Computer [A] from Codeforces

---
+++ 1362/A - Johnny and Ancient Computer/solution.cpp
@@ -0,0 +1,81 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define f1(i, a, b) for (int i = a; i <= b; i++)
+#define f2(i, a, b, c) for (int i = a; i <= b; i += c)
+#define vi vector<long long>
+#define vs vector<string>
+#define pii pair<int, int>
+#define vpii vector<PII>
+#define mii map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define asort(x) sort(all(x))
+#define int long long
+ 
+#define Its ios::sync_with_stdio(false);
+#define Dhruv cin.tie(nullptr);
+#define here cout.tie(nullptr);
+ 
+using namespace std;
+ 
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{
+    int a, b;
+    cin >> a >> b;
+    int ans = 0;
+    if (b > a)
+        swap(a, b);
+    if (a == b)
+        ans = 0;
+    else
+    {
+        if (a % b != 0)
+            ans = -1;
+        else
+        {
+            int x = a / b;
+            if ((x & x - 1) == 0)
+                ans = (((log2(x))) + 2) / 3;
+            else
+                ans = -1;
+        }
+    }
+ 
+    cout << ans << endl;
+}
+ 
+int32_t main()
+{
+    Its Dhruv here int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
