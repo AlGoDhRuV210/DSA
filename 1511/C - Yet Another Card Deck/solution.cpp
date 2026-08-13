45cf9e2ca290373812bf2a2d59abd7e5923e7a6f
AlGoDhRuV210 <dhruv.gupta.it01@gmail.com>
2026-08-13 23:38:08 +0530

    Add Yet Another Card Deck [C] from Codeforces

---
+++ 1511/C - Yet Another Card Deck/solution.cpp
@@ -0,0 +1,94 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i,n) for (int i = 0; i < n; i++)
+#define f1(i,a,b) for (int i = a; i <= b; i++)
+#define f2(i,a,b,c) for (int i = a; i <= b; i += c)
+#define vi vector<long long>
+#define vs vector<string>
+#define pii pair<int, int>
+#define vpii vector<pair<int,int>>
+#define mii map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define sort(x) sort(x.begin(),x.end())
+#define int long long
+ 
+ 
+ 
+ 
+#define Its ios::sync_with_stdio(false);
+#define Dhruv cin.tie(nullptr);
+#define here cout.tie(nullptr);
+ 
+using namespace std;
+ 
+ 
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+void solve()
+{   int n ,q ;
+    cin>>n>>q;
+    vi a(n+1); for(int i=1;i<=n;i++) cin>>a[i];
+    vpii v;
+    for (int i = n;i>=1; i--)
+    {
+        v.pb({a[i],i});
+    }
+    
+    while(q--)
+    {
+        int t;
+        cin>>t;
+        for (int i =v.size()-1; i>=0; i--)
+        {
+            if(v[i].first!=t)
+            {
+                v[i].second++;
+            }
+            else
+            {
+                cout<<v[i].second<<" ";
+                pii p ={t,1};
+                v.erase(v.begin()+i);
+                v.pb(p);
+                break;
+            }
+        }
+        
+ 
+    }
+    cout<<endl;
+ 
+}
+ 
+ 
+int32_t main() {
+    Its Dhruv here
+   
+     
+       solve();
+     
+ return 0; 
+}
