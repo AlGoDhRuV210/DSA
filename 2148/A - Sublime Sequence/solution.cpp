14052753e023fd89ce82ede1f82c278e4a257375
Dhruv kumar gupta <dhruv.gupta.it01@gmail.com>
2026-08-15 23:06:24 +0530

    Add Sublime Sequence [A] from Codeforces

---
+++ 2148/A - Sublime Sequence/solution.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n,x;
+        cin >> x >> n;
+        if (n%2==0) cout<<0;
+        else cout<<x;
+        cout<<endl;
+        
+    }
+ 
+    return 0;
+}
