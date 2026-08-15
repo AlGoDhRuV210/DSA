591d8922292968075e7744e49e051783a49d4c5a
Dhruv kumar gupta <dhruv.gupta.it01@gmail.com>
2026-08-15 23:07:55 +0530

    Add Prefix Max [B] from Codeforces

---
+++ 2185/B - Prefix Max/solution.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <math.h>
+ 
+using namespace std;
+ 
+/* Fast I/O */
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+ 
+/* Shortcuts */
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+ 
+/* Loops */
+#define fori(i, a, b) for (int i = a; i < b; i++)
+#define ford(i, a, b) for (int i = a; i >= b; i--)
+ 
+/* Constants */
+const int MOD = 1000000007;
+const long long INF = 1000000000000000000LL;
+ 
+/* Debug */
+#define debug(x) cerr << #x << " = " << x << endl;
+ 
+int main()
+{
+    fastio();
+ 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+ 
+            cin >> arr[i];
+        }
+        int* m = max_element(arr,arr+n);
+        cout<<n*(*m)<<endl;;
+          
+ 
+    }
+    return 0;
+}
