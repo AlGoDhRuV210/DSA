4f4f82ac683cf72d100716fd2aeb0ec25d8b5ebb
Dhruv kumar gupta <dhruv.gupta.it01@gmail.com>
2026-08-23 21:33:25 +0530

    Add Partitioning the Array [C] from Codeforces

---
+++ 1920/C - Partitioning the Array/solution.cpp
@@ -0,0 +1,183 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+using namespace std;
+ 
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define vi vector<long long>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define mii map<int, int>
+#define mci map<char, int>
+#define umci unordered_map<char, int>
+#define umii unordered_map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rev(x) reverse(all(x))
+#define asort(x) sort(all(x))
+#define dsort(x) sort(all(x), greater<>())
+#define int long long
+#define double long double
+#define endl '
+'
+ 
+long long gcd(long long a, long long b)
+{
+    while (b)
+    {
+        long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+long long lcm(long long a, long long b)
+{
+    return (a / gcd(a, b)) * b;
+}
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+ 
+class DisjointSet
+{
+private:
+    vector<int> parent, rank, size;
+ 
+public:
+    DisjointSet(int n)
+    {
+        parent.resize(n + 1);
+        rank.assign(n + 1, 0);
+        size.assign(n + 1, 1);
+        for (int i = 0; i <= n; i++)
+            parent[i] = i;
+    }
+ 
+    int findParent(int node)
+    {
+        if (parent[node] == node)
+            return node;
+        return parent[node] = findParent(parent[node]);
+    }
+ 
+    void unionByRank(int u, int v)
+    {
+        u = findParent(u);
+        v = findParent(v);
+        if (u == v)
+            return;
+        if (rank[u] < rank[v])
+            parent[u] = v;
+        else if (rank[u] > rank[v])
+            parent[v] = u;
+        else
+        {
+            parent[v] = u;
+            rank[u]++;
+        }
+    }
+ 
+    void unionBySize(int u, int v)
+    {
+        u = findParent(u);
+        v = findParent(v);
+        if (u == v)
+            return;
+        if (size[u] < size[v])
+        {
+            parent[u] = v;
+            size[v] += size[u];
+        }
+        else
+        {
+            parent[v] = u;
+            size[u] += size[v];
+        }
+    }
+};
+ 
+bool check(vi &a, int k)
+{
+    int g = abs(a[k] - a[0]);
+    int n = a.size();
+ 
+    for (int i = 0; i < k; i++)
+    {
+        int last = a[i];
+        for (int j = i + k; j < n; j += k)
+        {
+            g = gcd(g, abs(a[j] - last));
+            last = a[j];
+            if (g == 1)
+                return false;
+        }
+    }
+    return true;
+}
+void solve()
+{
+    int n;
+    cin >> n;
+    vi a(n);
+    f(i, n) cin >> a[i];
+ 
+    int ans = 0;
+ 
+    vi fact(n + 1, 0);
+    for (int i = 1; i < n; i++)
+    {
+        if (fact[i] != 0)
+            continue;
+        if (n % i == 0)
+        {
+            if (check(a, i))
+            {
+                int j = i;
+                while (j < n)
+                {
+                    fact[j] = 1;
+                    j += i;
+                }
+            }
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (fact[i] == 1 && n % i == 0)
+            ans++;
+    }
+    ans++; // for n
+    cout << ans << endl;
+}
+ 
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
