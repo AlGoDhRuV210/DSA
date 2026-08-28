64b4d287381de18258c6f1c4f83ed2d56d814e34
Dhruv kumar gupta <dhruv.gupta.it01@gmail.com>
2026-08-28 23:43:16 +0530

    Time: 0 ms (100%), Space: 10.4 MB (89.23%) - LeetHub

---
+++ LeetCode/Hard/3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target-08-28-2026-23-43-15.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+    string lexPalindromicPermutation(string s, string target) {
+        int n = s.size(), cnt[26] = {};
+
+        for (char c : s) cnt[c - 'a']++;
+
+        int odd = 0;
+        char mid = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (cnt[i] & 1) {
+                odd++;
+                mid = 'a' + i;
+            }
+            cnt[i] /= 2;
+        }
+
+        if (odd > 1) return "";
+
+        int m = n / 2;
+        string ans = s;
+        int pos = 0;
+
+        auto make = [&]() {
+            if (n & 1) ans[m] = mid;
+
+            for (int i = 0; i < m; i++)
+                ans[n - 1 - i] = ans[i];
+        };
+
+
+        while (pos < m && cnt[target[pos] - 'a']) {
+            ans[pos] = target[pos];
+            cnt[target[pos] - 'a']--;
+            pos++;
+        }
+
+
+        if (pos == m) {
+            make();
+            if (ans > target) return ans;
+        }
+
+
+        while (true) {
+
+
+            if (pos < m) {
+                for (int c = target[pos] - 'a' + 1; c < 26; c++) {
+
+                    if (!cnt[c]) continue;
+
+                    ans[pos] = 'a' + c;
+                    cnt[c]--;
+
+
+                    int j = pos + 1;
+
+                    for (int x = 0; x < 26; x++) {
+                        while (cnt[x]--) {
+                            ans[j++] = 'a' + x;
+                        }
+                    }
+
+                    make();
+                    return ans;
+                }
+            }
+
+            if (pos == 0) return "";
+
+            pos--;
+            cnt[target[pos] - 'a']++;
+        }
+    }
+};
