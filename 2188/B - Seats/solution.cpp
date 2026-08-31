ca4e6e1765651b7ae845a062b014d148e2c6e195
Dhruv kumar gupta <dhruv.gupta.it01@gmail.com>
2026-08-31 13:27:44 +0530

    Add Seats [B] from Codeforces

---
+++ 2188/B - Seats/solution.cpp
@@ -0,0 +1,220 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+using namespace std;
+ 
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <bitset>
+#include <tuple>
+#include <complex>
+#include <math.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define vi vector<long long>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define mii map<int, int>
+#define mci map<char, int>
+#define umci unordered_map<char, int>
+#define umii unordered_map<int, int>
+#define si set<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rev(x) reverse(all(x))
+#define asort(x) sort(all(x))
+#define dsort(x) sort(all(x), greater<>())
+#define int long long
+#define double long double
+#define endl '
+'
+#define coutyes cout << "YES" << endl
+#define coutno cout << "NO" << endl
+ 
+long long gcd(long long a, long long b)
+{
+    while (b)
+    {
+        long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+long long lcm(long long a, long long b)
+{
+    return (a / gcd(a, b)) * b;
+}
+const int MOD1 = 1000000007;
+ 
+class DisjointSet
+{
+private:
+    vector<int> parent, rank, size;
+ 
+public:
+    DisjointSet(int n)
+    {
+        parent.resize(n + 1);
+        rank.assign(n + 1, 0);
+        size.assign(n + 1, 1);
+        for (int i = 0; i <= n; i++)
+            parent[i] = i;
+    }
+ 
+    int findParent(int node)
+    {
+        if (parent[node] == node)
+            return node;
+        return parent[node] = findParent(parent[node]);
+    }
+ 
+    void unionByRank(int u, int v)
+    {
+        u = findParent(u);
+        v = findParent(v);
+        if (u == v)
+            return;
+        if (rank[u] < rank[v])
+            parent[u] = v;
+        else if (rank[u] > rank[v])
+            parent[v] = u;
+        else
+        {
+            parent[v] = u;
+            rank[u]++;
+        }
+    }
+ 
+    void unionBySize(int u, int v)
+    {
+        u = findParent(u);
+        v = findParent(v);
+        if (u == v)
+            return;
+        if (size[u] < size[v])
+        {
+            parent[u] = v;
+            size[v] += size[u];
+        }
+        else
+        {
+            parent[v] = u;
+            size[u] += size[v];
+        }
+    }
+};
+ 
+const int MOD = 998244353;
+const int MAXN = 300000;
+long long fact[MAXN + 1];
+long long invFact[MAXN + 1];
+long long power(long long a, long long b)
+{
+    long long res = 1;
+    while (b)
+    {
+        if (b & 1)
+            res = res * a % MOD;
+        a = a * a % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+long long inverse(long long a)
+{
+    return power(a, MOD - 2);
+}
+void initFactorials()
+{
+    fact[0] = 1;
+    for (int i = 1; i <= MAXN; i++)
+        fact[i] = fact[i - 1] * i % MOD;
+    invFact[MAXN] = inverse(fact[MAXN]);
+    for (int i = MAXN; i >= 1; i--)
+        invFact[i - 1] = invFact[i] * i % MOD;
+}
+long long nCr(int n, int r)
+{
+    if (r < 0 || r > n)
+        return 0;
+    return fact[n] * invFact[r] % MOD * invFact[n - r] % MOD;
+}
+long long nPr(int n, int r)
+{
+    if (r < 0 || r > n)
+        return 0;
+    return fact[n] * invFact[n - r] % MOD;
+}
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int ans = count(all(s), '1');
+    if (ans == 0)
+    {
+        ans = (n + 2) / 3;
+        cout << ans << endl;
+        return;
+    }
+ 
+    int len = 0;
+    int in = s.find('1');
+    ans += (in) / 3;
+    if (in % 3 == 2)
+        ans++;
+    int last = n - 1;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (s[i] == '1')
+        {
+            last = i;
+            break;
+        }
+    }
+    ans += (n - last - 1) / 3;
+    if ((n - last - 1) % 3 == 2)
+        ans++;
+ 
+    for (int i = s.find('1'); i < last; i++)
+    {
+        if (s[i] == '0')
+            len++;
+        else
+        {
+            ans += (len / 3);
+            len = 0;
+        }
+    }
+    ans += (len / 3);
+ 
+    cout << ans << endl;
+}
+ 
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    initFactorials();
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
